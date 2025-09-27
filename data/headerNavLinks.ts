810c20a32195c54a9d4e9fe8a2bb7b2617255297
methu-ship <mettusella@gmail.com>
2025-09-27 08:38:26 +0100

    Work section

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/', title: 'Home' },
   { href: '/about', title: 'About' },
   { href: '/blog', title: 'Blog' },
-  { href: 'https://sheddy.work', title: 'Work' },
+  { href: 'https://work-tau-amber.vercel.app/', title: 'Work' },
 ]
 
 export default headerNavLinks
